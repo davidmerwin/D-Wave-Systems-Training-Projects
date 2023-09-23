3ff45409097bb193054f91307c6b4de75320f672
David Jeffrey Merwin <davidmerwin1992@gmail.com>
2023-09-23 09:59:21 -0700

    Create Problemtwo.py

---
+++ Problemtwo.py
@@ -0,0 +1,10 @@
+'''
+Exercise 2
+To run the exercise 2 demo from the command-line, type the command:
+python two_different.py
+In this exercise, we want to penalize the two qubit solutions in which the qubits have the same values; we want to favor the two qubit solutions in which the qubits have different values.
+
+In the results; you should notice that the solutions $(0.1)$ and $(10)$ have lower energy than the solutions $(0$ ) and (1 1).
+'''
+![Antenna Graph](https://cdn.mathpix.com/snip/images/YV_bKgioJhuYz-vaRKUIjLRfYO9LtZrvLa373eWiUZU.original.fullsize.png)
+
